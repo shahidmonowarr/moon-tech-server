99326318897335aa6173224801347a4e46fead6f
shahidmonowarr <shahidmonowar21@gmail.com>
2022-12-10 20:21:36 +0600

    commit

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hdqkyze.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,7 +19,7 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
-    const db = client.db("moontech");
+    const db = client.db("moon");
     const productCollection = db.collection("product");
 
     app.get("/products", async (req, res) => {
